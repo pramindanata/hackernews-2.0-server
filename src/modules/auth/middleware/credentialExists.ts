46544cb9f5a58b3b7c039d0fec70436ae1695d73
pramindanata <pramindanata.eksa@gmail.com>
2020-03-13 16:17:27 +0800

    fix register middleware

--- src/modules/auth/middleware/credentialExists.ts
+++ src/modules/auth/middleware/credentialExists.ts
@@ -1,6 +1,6 @@
 import { Response, NextFunction } from 'express'
 import { Request } from 'express-serve-static-core'
-import { Not } from 'typeorm'
+import { Not, FindConditions } from 'typeorm'
 import User from '~/model/User'
 import * as I from '../interface'
 import * as RI from '~/interface'
@@ -13,17 +13,19 @@ export default (register: boolean) => async (
   let fail = false
   const { body } = req
   const messages = {} as RI.JoiErrorMessages
+  const usernameWhere: FindConditions<User> = { username: body.username }
+  const emailWhere: FindConditions<User> = { email: body.email }
+
+  if (!register && req.ctx.user) {
+    usernameWhere.id = Not(req.ctx.user.id)
+    emailWhere.id = Not(req.ctx.user.id)
+  }
+
   const userByUsernameP: Promise<User | undefined> = req.ctx.repo.user.findOne({
-    where: {
-      id: register ? undefined : Not(req.ctx.user?.id),
-      username: body.username,
-    },
+    where: usernameWhere,
   })
   const userByEmailP: Promise<User | undefined> = req.ctx.repo.user.findOne({
-    where: {
-      id: register ? undefined : Not(req.ctx.user?.id),
-      email: body.email,
-    },
+    where: emailWhere,
   })
   const [userByUsername, userByEmail] = await Promise.all([userByUsernameP, userByEmailP])
 
